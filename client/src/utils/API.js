1f1b3a2c857c4224784c0b0492d8df6135086eba
Caleb Chaney <calebc347@gmail.com>
2021-02-02 10:21:46 -0600

    Updated API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,14 +3,14 @@ require('dotenv').config();
 
 export default {
     getArtist: function (query) {
-        return axios.get("http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + query + "&limit=10&api_key=bf8d3ec29d790d8fdc71b82b15ad96a3&format=json");
+        return axios.get("http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + query + "&limit=15&api_key=bf8d3ec29d790d8fdc71b82b15ad96a3&format=json");
     },
     
     // Gets all Songs
     getUserSongs: function () {
         return axios.get("/api/songs");
     },
-
+    
     // Gets a Specific Song 
     getSong: function (id) {
         return axios.get("/api/songs/" + id);
